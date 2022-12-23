3d29f613db6d67af334ab917f7df7c86362c6a26
vdeo <vdeo@naoj.org>
2022-12-22 17:53:40 -1000

    2022/20

--- 2022/day20.py
+++ 2022/day20.py
@@ -11,10 +11,18 @@ SAMPLE = '1 2 -3 3 -2 0 4'.split()
 
 REAL = get_input(DAYDAY, 2022)
 
+class int_p:
+    def __init__(self, n: int) -> None:
+        self.n = n
+        
 
 class Day:
     def __init__(self, lines) -> None:
-        self.orig = [int(nn) for nn in lines]
+        self.orig = [int_p(int(nn)) for nn in lines]
+        for p in self.orig:
+            if p.n == 0:
+                self.zero = p
+                break
         self.deq = deque(self.orig.copy())
         self.l = len(self.orig)
 
@@ -24,18 +32,19 @@ class Day:
             self.deq.rotate(-idx % self.l)
             #assert self.deq[0] == val
             self.deq.popleft()
-            self.deq.insert(val % (self.l-1), val)
+            self.deq.insert(val.n % (self.l-1), val)
 
-        i0 = self.deq.index(0)
+        i0 = self.deq.index(self.zero)
         self.deq.rotate(-i0)
 
         #assert self.deq[0] == 0
 
 
-        return self.deq[1000 % self.l] + self.deq[2000 % self.l] + self.deq[3000 % self.l]
+        return self.deq[1000 % self.l].n + self.deq[2000 % self.l].n + self.deq[3000 % self.l].n
 
     def solve2(self, n:int = 10):
-        self.orig = [k * 811589153 for k in self.orig]
+        for p in self.orig:
+            p.n *= 811589153
         self.deq = deque(self.orig.copy())
 
         for k in range(n-1):
@@ -44,6 +53,8 @@ class Day:
 
 if __name__ == "__main__":
     t = Day(SAMPLE)
+    print(t.solve1())
     print(t.solve2())
     r = Day(REAL)
+    print(r.solve1())
     print(r.solve2())
